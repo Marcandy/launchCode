f8625877a58d698051996bd8acff8a8145008e9b
Marcandy <marcandy.nj@gmail.com>
2026-04-29 02:23:09 -0400

    environment set up

---
+++ programmingEnvironment/app.js
@@ -0,0 +1,7 @@
+// let item1 = 5;  // Price of item 1
+// let item2 = Number("10");  // Error: item2 is a string
+
+// let total = item1 + item2;
+// console.log("Total price:", total);
+
+console.log("Hello, World!");
